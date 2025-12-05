3ae971ef0b2cbcc9cb18e5eb188bd870ad35551d
ghifar1327 <givartorreto@gmail.com>
2025-12-05 22:47:28 +0700

    feat: dapatkan genre film untuk ditmapilkan

--- src/components/PartialSlideMovie.jsx
+++ src/components/PartialSlideMovie.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 export default function PartialSlideMovie() {
-  const [movies, setMovies] = useState([]);
   const apiKey = "b8892ef17f94739e8b1c3cb44d901d97";
+  const [movies, setMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -12,15 +13,16 @@ export default function PartialSlideMovie() {
       try {
         if (!response.ok) throw new Error(response.status, response.statusText);
         const data = await response.json();
-        console.log(data.results);
+        // console.log(data.results);
         const arrMovie = await data.results.map((item) => {
           return {
             title: item.title,
             image: item.poster_path,
             decription: item.overview,
+            genreId: item.genre_ids,
           };
         });
-        // console.log(arrMovie);
+        console.log(arrMovie);
 
         setMovies(arrMovie);
       } catch (error) {
@@ -28,17 +30,40 @@ export default function PartialSlideMovie() {
       }
     })();
   }, []);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`
+      );
+      const data = await res.json();
+      setGenres(data.genres);
+      console.log(data)
+    })();
+  }, []);
+
   return (
     <>
       <article className="hide-scrollbar flex overflow-x-scroll gap-[18px] mt-7 px-[180px]">
-        {movies.map((item, index) => {
+        {movies.slice(0,4).map((item, index) => {
           return (
             <figure ley={index} className="min-w-[265px] shrink-0">
               <img
                 src={`https://image.tmdb.org/t/p/w500${item.image}`}
                 className="w-[265px]"
               />
-              <h2 className="mt-2 text-sm wrap-break-word max-w-[200px]">{item.title}</h2>
+              <h2 className="mt-2 text-sm wrap-break-word max-w-[200px]">
+                {item.title}
+              </h2>
+              <section className="flex gap-3">{item.genreId.slice(0,2).map((id)=>{
+                const genre = genres.find((g)=> g.id === id)
+                return(
+                  <p key={id} className="border">
+                    {genre?.name}
+                    {" "}
+                  </p>
+                )
+              })}</section>
             </figure>
           );
         })}
